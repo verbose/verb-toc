d96fb5c91cff58419aaf71e96d213ea4f7d3c237
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-11 22:39:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * verb-toc <https://github.com/jonschlinkert/verb-toc>
+ *
+ * Copyright (c) 2016, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function() {
+  return function() {
+
+  };
+};
